2fcce3ab2aea5186dd749c510c86f19c95ae084f
Juan Diego Salazar Velasquez <diego.salazar@ias.com.co>
2023-02-12 18:17:50 -0500

    verificacion de nombre y documento ok!

--- src/main/java/co/com/ias/settlement/domain/model/employee/EmployeeName.java
+++ src/main/java/co/com/ias/settlement/domain/model/employee/EmployeeName.java
@@ -1,10 +1,15 @@
 package co.com.ias.settlement.domain.model.employee;
 
+import org.springframework.util.Assert;
+
 public class EmployeeName {
 
     private final String value;
 
     public EmployeeName(String value) {
+        Assert.notNull(value, "El nombre no puede ser nulo");
+        Assert.hasLength(value, "El nombre no puede ser vacio");
+        Assert.isTrue(value.length() <= 50, "El nombre no puede tener mas de 50 caracteres");
         this.value = value;
     }
 
--- src/main/java/co/com/ias/settlement/domain/model/employee/IdentificationId.java
+++ src/main/java/co/com/ias/settlement/domain/model/employee/IdentificationId.java
@@ -1,10 +1,14 @@
 package co.com.ias.settlement.domain.model.employee;
 
+import org.springframework.util.Assert;
+
 public class IdentificationId {
 
     private final String value;
 
     public IdentificationId(String value) {
+        Assert.notNull(value, "La cédula no puede ser nula");
+        Assert.isTrue(value.length() >= 7 && value.length() <= 17, "La cédula debe tener entre 7 y 15 caracteres");
         this.value = value;
     }
 
