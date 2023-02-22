80b4bcfa0ec8e3736b9aea80fe5a741fc72fa744
Juan Diego Salazar Velasquez <diego.salazar@ias.com.co>
2023-02-22 13:56:33 -0500

    test updateEmployee

--- src/test/java/co/com/ias/settlement/infrastructure/entrypoints/employee/EmployeeEntryPointTest.java
+++ src/test/java/co/com/ias/settlement/infrastructure/entrypoints/employee/EmployeeEntryPointTest.java
@@ -4,8 +4,10 @@ import co.com.ias.settlement.domain.model.employee.*;
 import co.com.ias.settlement.domain.model.employeestate.EmployeeState;
 import co.com.ias.settlement.domain.model.employeestate.StateId;
 import co.com.ias.settlement.domain.model.employeestate.StateName;
+import co.com.ias.settlement.domain.model.updateemployee.UpdateEmployee;
 import co.com.ias.settlement.domain.usecase.employee.EmployeeUseCase;
 import co.com.ias.settlement.infrastructure.entrypoints.employee.dto.EmployeeDTO;
+import co.com.ias.settlement.infrastructure.entrypoints.employee.dto.UpdateEmployeeDTO;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -20,6 +22,8 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.mockito.ArgumentMatchers.any;
@@ -49,8 +53,8 @@ class EmployeeEntryPointTest {
                 new EmployeeName("Juan"),
                 new ContractStartDate(LocalDate.parse("2020-01-01")),
                 new EmployeePosition("Desarrollador"),
-                new Salary(2000000D),
-                null,
+                new Salary(3000000D),
+                new UpdateEmployDate(LocalDate.parse("2022-01-16")),
                 new EmployeeState(
                         new StateId(1),
                         new StateName("Activo")
@@ -73,8 +77,6 @@ class EmployeeEntryPointTest {
                         .content(mapper.writeValueAsString(employeeDTO)))
                 .andExpect(content().string(containsString((employeeDTO.getIdentificationId()))))
                 .andExpect(MockMvcResultMatchers.status().isCreated());
-
-
     }
 
     @Test
@@ -89,11 +91,31 @@ class EmployeeEntryPointTest {
     }
 
     @Test
-    void findEmployees() {
+    @DisplayName("FindEmployees ok")
+    void findEmployees() throws Exception {
+        List<Employee> employees = Arrays.asList(employee);
+        List<EmployeeDTO> employeeDTOS = Arrays.asList(employeeDTO);
+        when(this.employeeUseCase.findEmployees()).thenReturn(employees);
+
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/employee"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(content().string(containsString(employeeDTOS.get(0).getIdentificationId())));
     }
 
     @Test
-    void updateEmployee() {
+    void updateEmployee() throws Exception {
+        UpdateEmployeeDTO updateEmployeeDTO = new UpdateEmployeeDTO("1234567", "Desarrollador", 3000000D, "2022/16/01");
+        UpdateEmployee updateEmployee = UpdateEmployeeDTO.toDomain(updateEmployeeDTO);
+
+        when(this.employeeUseCase.updateEmployee(any(UpdateEmployee.class))).thenReturn(employee);
+
+        this.mockMvc.perform(MockMvcRequestBuilders.put("/employee")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(updateEmployeeDTO)))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(content().string(containsString(employeeDTO.getUpdateEmployeeDate())))
+                .andExpect(content().string(containsString(employeeDTO.getEmployeeState().getStateName())));
+
     }
 
     @Test
