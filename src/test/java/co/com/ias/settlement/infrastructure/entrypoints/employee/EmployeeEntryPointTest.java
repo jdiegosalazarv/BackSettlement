1d26b0cd1d77e10f2089c5d57542637c38d2465f
Juan Diego Salazar Velasquez <diego.salazar@ias.com.co>
2023-02-21 23:53:28 -0500

    test adapter employee y a entrypoint employee

---
+++ src/test/java/co/com/ias/settlement/infrastructure/entrypoints/employee/EmployeeEntryPointTest.java
@@ -0,0 +1,95 @@
+package co.com.ias.settlement.infrastructure.entrypoints.employee;
+
+import co.com.ias.settlement.domain.model.employee.*;
+import co.com.ias.settlement.domain.model.employeestate.EmployeeState;
+import co.com.ias.settlement.domain.model.employeestate.StateId;
+import co.com.ias.settlement.domain.model.employeestate.StateName;
+import co.com.ias.settlement.domain.usecase.employee.EmployeeUseCase;
+import co.com.ias.settlement.infrastructure.entrypoints.employee.dto.EmployeeDTO;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.time.LocalDate;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+@WebMvcTest(EmployeeEntryPoint.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class EmployeeEntryPointTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    EmployeeUseCase employeeUseCase;
+
+    Employee employee;
+    EmployeeDTO employeeDTO;
+
+    ObjectMapper mapper;
+
+    @BeforeEach
+    void beforeEach() {
+
+        employee = new Employee(
+                new IdentificationId("1234567"),
+                new EmployeeName("Juan"),
+                new ContractStartDate(LocalDate.parse("2020-01-01")),
+                new EmployeePosition("Desarrollador"),
+                new Salary(2000000D),
+                null,
+                new EmployeeState(
+                        new StateId(1),
+                        new StateName("Activo")
+                )
+        );
+
+        employeeDTO = EmployeeDTO.fromDomain(employee);
+
+        mapper = new ObjectMapper();
+
+    }
+
+    @Test
+    @DisplayName("Guardar empleado ok")
+    void saveEmployee() throws Exception {
+        when(this.employeeUseCase.saveEmployee(any(Employee.class))).thenReturn(employee);
+
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/employee")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(employeeDTO)))
+                .andExpect(content().string(containsString((employeeDTO.getIdentificationId()))))
+                .andExpect(MockMvcResultMatchers.status().isCreated());
+
+
+    }
+
+    @Test
+    void findEmployeeById() {
+    }
+
+    @Test
+    void findEmployees() {
+    }
+
+    @Test
+    void updateEmployee() {
+    }
+
+    @Test
+    void deleteEmployee() {
+    }
+}
